e762108db55c0baf74e75b7d38e980ca2a89f82a
tpendrell <87634526+tpendrell@users.noreply.github.com>
2026-04-17 09:45:24 -0600

    Set up React application with NebraskaBudgetDashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NebraskaBudgetDashboard from './NebraskaPublicBudgetDashboard'
 
+// This connects your React code to the <div id="root"></div> in your index.html
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NebraskaBudgetDashboard />
+  </React.StrictMode>,
+)
